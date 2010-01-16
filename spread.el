3d01385aa4719133405bd9e8ac0480349b9e2eea
Thomas Munro <munro@sabre.config>
2010-01-16 10:13:02 +0000

    Added GPL notice

--- spread.el
+++ spread.el
@@ -4,6 +4,19 @@
 ;; Author: Thomas Munro <munro@ip9.org>
 ;; Keywords: spread, protocol, multicast
 
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 ;;; Commentary:
 ;; 
 ;; This is just a little warm-up exercise in network programming.  See
